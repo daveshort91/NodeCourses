c7ac9566d53837ac61db79210ea8beb6988c2cbf
david short <sho13014@byui.edu>
2018-07-10 00:53:57 -0600

    bug fixes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var express = require("express");
+const path = require('path')
 var app = express();
 
 // const {Pool} = require("pg");
-
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.set("port", (process.env.PORT || 3000));
 
-app.get("/home", home);
+app.get('/', (req, res) => res.render('pages/index'));
+app.get("/home", (req, res) => res.render('pages/index'));
 app.get("/addCourse", addCourse);
-app.get("/registerUser", registerUser);
+app.get("/registerUser", (req, res) => res.render('pages/register'));
 app.get("/registerCourse", registerCourse);
 app.get("/course", course);
+app.get("/myCourses", myCourses);
 
 app.listen(app.get("port"), function(){
   console.log("Listening on port:", app.get("port"));
@@ -33,4 +38,8 @@ function registerCourse(req, res) {
 
 function course(req, res) {
   console.log("this is a course");
+}
+
+function myCourses(req, res) {
+
 }
